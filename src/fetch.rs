a6596bc5970a538828c82ef733485eb74706c24e
www6v <www6v@tuhuadmindeMacBook-Air.local>
2022-01-02 18:52:43 +0800

    init

---
+++ src/fetch.rs
@@ -0,0 +1,6 @@
+// use anyhow::{anyhow, Result};
+//  use clap::{AppSettings, Clap};
+//  use colored::*;
+//  use mime::Mime;
+//  use reqwest::{header, Client, Response, Url};
+//  use std::{collections::HashMap, str::FromStr};
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
+use std::fs;
+
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+
+    fetch();
+}
+
+fn fetch() {
+    let url = "https://www.rust-lang.org/";
+    let output = "rust.md";
+    
+    println!("Fetching url: {}", url);
+    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    
+    println!("Converting html to markdown...");
+    let md = html2md::parse_html(&body);
+    
+    fs::write(output, md.as_bytes()).unwrap();  
+}
+
+    
