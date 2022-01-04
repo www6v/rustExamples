941a130fe039b2add2d47ac6fb29a4913832dd38
www6v <www6v@tuhuadmindeMacBook-Air.local>
2022-01-04 23:52:42 +0800

    moved

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
 
     fetchIgnoreError();
     fetchWithError();
+    moveTest();
 }
 
 fn fetchIgnoreError() {
@@ -40,5 +41,16 @@ fn fetchWithError() -> Result<(), Box<dyn std::error::Error>> {
     Ok(()) 
 }
 
+fn moveTest() {
+    let data = vec![1, 2, 3, 4];
+    let data1 = data;
+    println!("sum of data1: {}", sum(data1));
+    // println!("data1: {:?}", data1); // error1
+    // println!("sum of data: {}", sum(data)); // error2   
+} 
+
+fn sum(data: Vec<u32>) -> u32 {
+     data.iter().fold(0, |acc, x| acc + x) 
+}
 
     
