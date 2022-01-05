509cbaeaa29f1740a31193677496aa1645949554
wangwei18 <wangwei18@tuhu.cn>
2022-01-05 19:47:44 +0800

    copy trait

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
 
     fetchIgnoreError();
     fetchWithError();
+    copyTest();
 }
 
 fn fetchIgnoreError() {
@@ -41,4 +42,48 @@ fn fetchWithError() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 
-    
+
+
+fn is_copy<T: Copy>() {} 
+
+///  实现了 copy trait
+fn types_impl_copy_trait() { 
+    is_copy::<bool>(); 
+    is_copy::<char>(); 
+    // all iXX and uXX, usize/isize, fXX implement Copy trait 
+    is_copy::<i8>(); 
+    is_copy::<u64>(); 
+    is_copy::<i64>(); 
+    is_copy::<usize>(); 
+    // function (actually a pointer) is Copy 
+    is_copy::<fn()>(); 
+    // raw pointer is Copy 
+    is_copy::<*const String>(); 
+    is_copy::<*mut String>(); 
+    // immutable reference is Copy 
+    is_copy::<&[Vec<u8>]>(); 
+    is_copy::<&String>(); 
+    // array/tuple with values which is Copy is Copy 
+    is_copy::<[u8; 4]>(); 
+    is_copy::<(&str, &str)>(); 
+}
+
+///  没有实现 copy trait
+// fn types_not_impl_copy_trait() { 
+//     // unsized or dynamic sized type is not Copy 
+//     is_copy::<str>(); 
+//     is_copy::<[u8]>(); 
+//     is_copy::<Vec<u8>>(); 
+//     is_copy::<String>(); 
+//     // mutable reference is not Copy 
+//     is_copy::<&mut String>();
+
+//     // array / tuple with values that not Copy is not Copy 
+//     is_copy::<[Vec<u8>; 4]>(); 
+//     is_copy::<(String, u32)>(); 
+// }
+
+fn copyTest() { 
+    types_impl_copy_trait(); 
+    // types_not_impl_copy_trait(); 
+} 
